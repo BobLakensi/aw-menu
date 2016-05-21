9a9b8912d4301f278a5f510753493e58fbb64c65
Monte <BobLakensi@users.noreply.github.com>
2016-05-21 15:33:35 -0700

    Added buttons in preload

--- scenes/Menu.js
+++ scenes/Menu.js
@@ -1,6 +1,13 @@
 AG.Menu = function(){};
 
 AG.Menu.prototype = {
+  preload: function (){
+    game.load.spritesheet("creditsButtonTemp", "Assets/Sprites/creditsButtonTemp.png", 314, 124);
+    game.load.spritesheet("playButtonTemp", "Assets/Sprites/creditsButtonTemp.png", 314, 125 );
+    game.load.spritesheet("settingsButtonTemp", "Assets/Sprites/creditsButtonTemp.png", 314, 127);
+
+  },
+	
   create: function(){
     console.log('You are in the Menu state');
     game.stage.backgroundColor = '#fff';
