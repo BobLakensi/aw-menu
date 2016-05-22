8cce86a99ee7cfc559a4e669a2a37112cb2afb5d
Monte <BobLakensi@users.noreply.github.com>
2016-05-21 21:32:58 -0700

    Added player imgs, sprite, and object

--- scenes/Map.js
+++ scenes/Map.js
@@ -1,9 +1,5 @@
 AG.Map = function(){};
-
-
   
-
-
 var helper = {
   move: function () {
     console.log("moving")
@@ -11,9 +7,16 @@ var helper = {
 
 }
 
+//player object to help manage player stuff
+var playerStuff = {
+  
+}
+
 AG.Map.prototype = {
   preload: function () {
-    
+    game.load.image("robFrontIMG", "Assets/Sprites/robEagle.png")
+    game.load.image("robsideIMG", "Assets/Sprites/robSide.png")
+    game.load.image("robBackIMG", "Assets/Sprites/robBack.png")
     
   },
   
@@ -21,9 +24,17 @@ AG.Map.prototype = {
     console.log('You are in the Map state');
     game.stage.backgroundColor = '#0000FF';
     
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    playerStuff.player = game.add.sprite (game.centerX, game.centerY, "robFrontIMG");
+    playerStuff.player.scale.x = 0.3;
+    playerStuff.player.scale.y = 0.3;
+    
+    game.physics.enable(playerStuff.player);
 
   },
   update: function(){
+    //WASD keys to do stuff
     game.input.keyboard.addKey(Phaser.Keyboard.W).onDown.add(helper.move, this);
     game.input.keyboard.addKey(Phaser.Keyboard.A).onDown.add(helper.move, this);
     game.input.keyboard.addKey(Phaser.Keyboard.S).onDown.add(helper.move, this);
