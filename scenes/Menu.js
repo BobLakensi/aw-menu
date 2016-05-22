dc52924e742845c93b875f1053279c65dda7f9e8
Monte <BobLakensi@users.noreply.github.com>
2016-05-21 17:15:32 -0700

    Buttons now fully centered

--- scenes/Menu.js
+++ scenes/Menu.js
@@ -28,8 +28,12 @@ AG.Menu.prototype = {
     buttonHelper.playButton = game.add.button(game.world.centerX, game.world.height + 50, "playButtonTemp", buttonHelper.startGame, null, 1, 2, 3, 1);
     buttonHelper.settingsButton = game.add.button(game.world.centerX, game.world.height + 50, "settingsButtonTemp", null, null, 1, 2, 3, 1);
     buttonHelper.creditsButton = game.add.button(game.world.centerX, game.world.height + 50, "creditsButtonTemp", null, null, 1, 2, 3, 1);
+    
+    //fix button positions
+    buttonHelper.playButton.x = buttonHelper.playButton.x - buttonHelper.playButton.width/2;
+    buttonHelper.settingsButton.x = buttonHelper.settingsButton.x - buttonHelper.settingsButton.width/2;
+    buttonHelper.creditsButton.x = buttonHelper.creditsButton.x - buttonHelper.creditsButton.width/2;
 
-  
     //tween buttons
     game.add.tween(buttonHelper.playButton).to( { y: game.world.centerY/3 }, buttonHelper.durationOfAnim, buttonHelper.easeAnim, true, buttonHelper.delayAim);
     game.add.tween(buttonHelper.settingsButton).to( { y: game.world.centerY*2/3 }, buttonHelper.durationOfAnim, buttonHelper.easeAnim, true, buttonHelper.delayAim*2);
